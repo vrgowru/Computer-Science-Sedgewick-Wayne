c98d72652b91254ce44f82ef21ae35ce607785bc
vrgowru <vrgowru@gmail.com>
2020-05-27 16:57:15 -0400

    Initial Commit

---
+++ src/src/chapter_1/HelloWorld.java
@@ -0,0 +1,26 @@
+package chapter_1;
+
+/************************************************************************************
+ * These are set of programs written for Computer Science: Programming with a Purpose
+ * course on coursera linked below
+ * @Link - https://www.coursera.org/learn/cs-programming-java/home/welcome
+ * The programs contain examples, solutions to chapter exercises
+ * programming assignments and are authored by
+ *
+ * @author Venkateshwara Gowru
+ *
+ * @Problem Statement - Write a simple program to print Hello, World!!
+ *
+ * @ Output - Prints Hello, World!!
+ *
+ ************************************************************************************/
+
+public class HelloWorld {
+
+    /* Java main method that initializes the code in memory and executes the functions
+     * /operations mentioned in this block of code
+     * */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!");
+    }
+}
